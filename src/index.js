24f49c5f1d492c9f10f49eca67cce3529e6d5311
Nigel Alford <nigel@callrail.com>
2019-05-23 16:18:29 -0400

    starting subCollection support

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ var firebase = require('firebase');
  * @returns {{teams: {get, save, all}, channels: {get, save, all}, users: {get, save, all}}}
  */
 module.exports = function(config) {
-
     if (!config) {
         throw new Error('configuration is required.');
     }
@@ -19,7 +18,6 @@ module.exports = function(config) {
     var app = null,
         database = null,
         settings = null;
-
 
     if (config.database) {
         database = config.database;
@@ -33,13 +31,14 @@ module.exports = function(config) {
         }
 
         if (!config.settings) {
-            config.settings = {/* your settings... */ timestampsInSnapshots: true};
+            config.settings = {
+                /* your settings... */ timestampsInSnapshots: true
+            };
         }
 
         app = firebase.initializeApp(config);
         database = app.firestore();
         settings = database.settings(config.settings);
-
     }
 
     var rootRef = database,
@@ -48,14 +47,14 @@ module.exports = function(config) {
 
     // Implements required API methods
     for (var i = 0; i < collections.length; i++) {
-        storage[collections[i]] = getStorageObj(rootRef.collection(collections[i]));
+        storage[collections[i]] = getStorageObj(
+            rootRef.collection(collections[i])
+        );
     }
     // console.log('config', config);
     // console.log('storage', storage);
     return storage;
 };
-
-
 
 /**
  * Function to generate a storage object for a given namespace
@@ -67,7 +66,10 @@ function getStorageObj(collection) {
     return {
         get: get(collection),
         save: save(collection),
+        nestedSave: nestedCollectionSave(collection),
+        nestedQuery: nestedCollectionQuery(collection),
         all: all(collection),
+        where: where(collection),
         ref: collection
     };
 }
@@ -80,9 +82,13 @@ function getStorageObj(collection) {
  */
 function get(firebaseRef) {
     return function(id, cb) {
-        firebaseRef.doc(id).get().then(function(snapshot) {
-            cb(null, snapshot.data());
-        }).catch(err => cb(err));
+        firebaseRef
+            .doc(id)
+            .get()
+            .then(function(snapshot) {
+                cb(null, snapshot.data());
+            })
+            .catch(err => cb(err));
     };
 }
 
@@ -97,13 +103,16 @@ function save(firebaseRef) {
         var firebase_update = {};
         firebase_update[data.id] = data;
 
-        firebaseRef.doc(data.id).set(data, {merge: true}).then(doc => {
-            if (!doc) {
-                cb('No such document!', null);
-            } else {
-                cb(null, doc);
-            }
-        });
+        firebaseRef
+            .doc(data.id)
+            .set(data, { merge: true })
+            .then(doc => {
+                if (!doc) {
+                    cb('No such document!', null);
+                } else {
+                    cb(null, doc);
+                }
+            });
     };
 }
 
@@ -128,5 +137,79 @@ function all(firebaseRef) {
 
             cb(null, records.docs.map(result => result.data()));
         });
+    };
+}
+
+/**
+ * Given a firebase ref, will return a function that will execute a specified where search.
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The all function
+ */
+function where(firebaseRef) {
+    return (query, cb) => {
+        firebaseRef
+            .where(query[0], query[1], query[2])
+            .get()
+            .then(records => {
+                if (records.empty) {
+                    return cb(null, []);
+                }
+
+                cb(null, records.docs.map(result => result.data()));
+            });
+    };
+}
+
+/**
+ * Given a firebase ref, will return a function that will save an object. The object must have an id property
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The save function
+ */
+function nestedCollectionSave(firebaseRef) {
+    return function(data, cb) {
+        var firebase_update = {};
+        firebase_update[data.id] = data;
+
+        const document = data;
+        // delete document.subCollection;
+        // delete document.id;
+
+        firebaseRef
+            .doc(data.docId)
+            .collection(data.subCollection)
+            .doc(data.id)
+            .set(document, { merge: true })
+            .then(doc => {
+                if (!doc) {
+                    cb('No such document!', null);
+                } else {
+                    cb(null, doc);
+                }
+            });
+    };
+}
+
+/**
+ * Given a firebase ref, will return a function that will save an object. The object must have an id property
+ *
+ * @param {Object} firebaseRef A reference to the firebase Object
+ * @returns {Function} The save function
+ */
+function nestedCollectionQuery(firebaseRef) {
+    return (data, cb) => {
+        firebaseRef
+            .doc(data.docId)
+            .collection(data.subCollection)
+            .where(data.query[0], data.query[1], data.query[2])
+            .get()
+            .then(records => {
+                if (records.empty) {
+                    return cb(null, []);
+                }
+
+                cb(null, records.docs.map(result => result.data()));
+            });
     };
 }
